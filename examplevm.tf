29e489bba63453cacf9a9d53bf7fe484e9edd41d
Marius.Sandbu <marius.sandbu@evry.com>
2021-02-09 14:30:16 +0100

    Removed example extensions

--- examplevm.tf
+++ examplevm.tf
@@ -67,38 +67,5 @@ resource "azurerm_windows_virtual_machine" "winvm01-vm" {
    backup_policy_id    = azurerm_backup_policy_vm.bp01.id
  }
 
-# resource "azurerm_virtual_machine_extension" "monitor-DependencyAgent-agent" {
-#   name                       = "vmext-monitorDepAgent"
-#   virtual_machine_id         = azurerm_windows_virtual_machine.winvm01-vm.id
-#   publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
-#   type                       = "DependencyAgentWindows"
-#   type_handler_version       = "9.5"
-#   auto_upgrade_minor_version = true
-
-#   settings = <<SETTINGS
-#         {
-#           "workspaceId": "ffd3d30a-ec0e-403f-939e-9db4a91ab7df"
-#         }
-# SETTINGS
-
-#   protected_settings = <<PROTECTED_SETTINGS
-#         {
-#           "workspaceKey": "7pAcMJ+aLk4F84EI+bcUbQCz8VoLPj002q31c6JxjFzQ3PIbl/nekiINqv7XcE5f5D7nse9Ch2qcdRh3J6Z6LQ=="
-#         }
-# PROTECTED_SETTINGS
-#   tags = {
-#     environment = "production"
-#     role        = "server"
-#   }
-# }
-
-# resource "azurerm_virtual_machine_extension" "monitor-VMInsightHealth" {
-#   name                       = "vmext-monitorDepAgent"
-#   virtual_machine_id         = azurerm_windows_virtual_machine.winvm01-vm.id
-#   publisher                  = "Microsoft.Azure.Monitor.VirtualMachines.GuestHealth"
-#   type                       = "GuestHealthWindowsAgent"
-#   type_handler_version       = "1.0"
-#   auto_upgrade_minor_version = true
-# }
 
 
